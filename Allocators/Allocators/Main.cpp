1065ad25c6935f8d28acc7027737d8af71b12d9f
ahmed elnaggar <ahmed.hamed9@msa.edu.eg>
2026-01-31 14:29:33 +0200

    simple alloc (some stuff is there for my stickass compiler only but the ideas are simple)

---
+++ Allocators/Allocators/Main.cpp
@@ -0,0 +1,44 @@
+﻿#include<iostream>
+#include<vector>
+
+#include<limits>
+
+template<typename type>
+struct CusAlloc {
+	using value_type = type;
+
+	CusAlloc() = default;
+	
+
+	template<typename U>
+	CusAlloc(const CusAlloc<U>&) noexcept {}
+
+
+	type* allocate(size_t n) {
+		std::cout << "allocating size of -> " << n << " elements" << std::endl;
+		return static_cast<type*>(::operator new (sizeof(type) * n));
+	}
+
+	void deallocate(type* p, size_t n) {
+		std::cout << "deallocating " << n << " elements" << std::endl;
+	    ::operator delete(p);
+	}
+
+
+};
+int main() {
+	std::vector<int, CusAlloc<int> >vec;
+	vec.push_back(69);
+	vec.push_back(69);
+	vec.push_back(69);
+	vec.push_back(69);
+	vec.push_back(69);
+	vec.push_back(69);
+	vec.push_back(69);
+	vec.push_back(69);
+	vec.push_back(69);
+
+	for (const auto& i : vec) {
+		std::cout << i << " ";
+	}
+}
